f27fdc01811bc0fa0961c5e706cef86e9b91d184
utkarsh311wws <utkarshujjwal51@gmail.com>
2022-10-30 09:47:24 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const hello = require("./routes/hello");
+
+// middlewares
+app.use(express.json());
+
+// adding routes
+app.use("/hello", hello);
+
+// port
+const port = process.env.PORT || 5500;
+
+app.listen(port, () => console.log(`Listening on Port: ${port}`));
